f99c917e7d138693c7a5ccb16be2c0d1be1678b8
Alice Huang <alice@expertise.com>
2017-07-22 14:35:41 -0700

    Pagination works

--- client/components/pages/tableBlock.js
+++ client/components/pages/tableBlock.js
@@ -17,7 +17,7 @@ export default class TableBlock extends React.Component {
   state = {
     sortedColumn: null,
     isAscending: false,
-    index: 1
+    index: 0
   };
 
   onClick = ::this.onClick
@@ -32,7 +32,7 @@ export default class TableBlock extends React.Component {
   nextPage = ::this.nextPage
   nextPage() {
     this.setState({
-      index: this.state.index + 2
+      index: this.state.index + 25
     });
   }
 
@@ -40,7 +40,7 @@ export default class TableBlock extends React.Component {
   prevPage() {
     if (this.state.index >= 2) {
       this.setState({
-          index: this.state.index - 2
+          index: this.state.index - 25
       });
     }
   }
@@ -48,39 +48,32 @@ export default class TableBlock extends React.Component {
   render() {
     const { isSortable, columns, rows } = this.props;
     console.log(this.state.index)
-    const headers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const headers = ["Job Title", "Female Avg Hourly Wage", "Male Avg Hourly Wage"];
 
-    let wages = [
-      [ "Accountant", "30.58", "23", "88.96", "30.28", "7", "85.71", "30.51", "30", "88.20", "101.00", null ],
-      [ "Accountant,Prin", "38.75", "23", "59.96", "37.64", "7", "7.00", "38.49", "30", "47.60", "102.95", null ],
-      [ "Accountant,Sr", "34.48", "23", "60.61", "33.89", "6", "43.67", "34.36", "29", "57.10", "101.75", null ],
-      [ "Act Exec", "43.10", "4", "146.00", "42.02", "7", "106.29", "42.42", "11", "120.73", "102.56", null ],
-      [ "Actg Tech I", "22.56", "1", "156.00", null, null, null, "22.56", "1", "156.00", null, "no men in title" ],
-      [ "Actg Tech I-BU", "22.56", "10", "81.80", "22.56", "2", "35.50", "22.56", "12", "74.08", "100.00", null ],
-      [ "Actg Tech II", "23.98", "7", "112.71", "24.35", "1", "75.00", "24.03", "8", "108.00", "98.49", null ],
-      [ "Actg Tech II-BU", "24.19", "74", "103.23", "24.21", "18", "134.50", "24.20", "92", "109.35", "99.95", null ],
-      [ "Actg Tech III", "26.64", "7", "51.57", null, null, null, "26.64", "7", "51.57", null, "no men in title" ],
-      [ "Actg Tech III-BU", "26.69", "32", "89.97", "26.18", "5", "51.20", "26.63", "37", "84.73", "101.96", null ],
+    const inputData = data.data.slice(0,100);
+    let outputData = inputData.map((row) => {
+      return [row[8], row[9], row[15]];
+    });
+    outputData = outputData.slice(this.state.index, this.state.index+25);
 
-    ];
-    wages = wages.slice(this.state.index, this.state.index+2)
     const componentTypes = {
       string: (item) => (item),
       number: (item) => (item)
     };
-    // console.log(pagination());
+    
+    console.log(outputData)
     return (
       <div>
         <table className="ws_data_table" >
           <thead className="ws_data_table th">
             <tr>
               {headers.map((header, i) => {
-                return (<th key={i}>header</th>)
+                return (<th key={i}>{header}</th>)
               })}
             </tr>
           </thead>
           <tbody>
-            {wages.map((row, i) => {
+            {outputData.map((row, i) => {
               return(<tr key={i}>{row.map((r, j) => {
                 return (<td key={j}>{r}</td>)
               })}
